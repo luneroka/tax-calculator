d0e3bb37c535ceb00593a2f4ee888b9407011365
Luneroka <luneroka@gmail.com>
2025-07-29 21:21:30 +0200

    Create setOutput helper to abstract DOM update pattern

--- js/core/helper.js
+++ js/core/helper.js
@@ -1,3 +1,14 @@
 export function formatNumber(num) {
   return new Intl.NumberFormat('fr-FR').format(num);
 }
+
+export function setOutput(id, value, round = false) {
+  const el = document.querySelector(id);
+  if (el) {
+    if (isNaN(value) || value === undefined || value === null) {
+      el.textContent = '';
+    } else {
+      el.textContent = formatNumber(roud ? Math.round(value) : value);
+    }
+  }
+}
